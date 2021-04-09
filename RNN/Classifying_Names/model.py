e19ac2db3f751bbec216bdb6ecc3231bf522dceb
Gladiator07 <atharvaaingle@gmail.com>
2021-04-09 10:20:11 +0000

    model

--- RNN/Classifying_Names/model.py
+++ RNN/Classifying_Names/model.py
@@ -7,16 +7,18 @@ class RNN(nn.Module):
         super(RNN, self).__init__()
 
         self.hidden_size = hidden_size
+
         self.i2h = nn.Linear(input_size + hidden_size, hidden_size)
-        self.i2o = nn.Linear(input_size + hidden_size, output_size)
+        self.i20 = nn.Linear(input_size, hidden_size, output_size)
         self.softmax = nn.LogSoftmax(dim=1)
     
     def forward(self, input, hidden):
         combined = torch.cat((input, hidden), 1)
         hidden = self.i2h(combined)
-        output = self.i2o(combined)
+        output = self.i20(combined)
         output = self.softmax(output)
+
         return output, hidden
     
     def initHidden(self):
-        return Variable(torch.zeros(1, self.hidden_size))
+        return torch.zeros(1, self.hidden_size)
