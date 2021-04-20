d1b31d8f2878950ba9c08b272cc9dd7ca7604df4
Gladiator07 <atharvaaingle@gmail.com>
2021-04-20 10:57:30 +0000

    hyperparams

--- Seq_to_Seq/german_to_english_seq_to_seq/seq2seq.py
+++ Seq_to_Seq/german_to_english_seq_to_seq/seq2seq.py
@@ -1,8 +1,9 @@
+from spacy.util import load_model
 from torch.utils.tensorboard import SummaryWriter  # to print to tensorboard
 import random
 import spacy
 import numpy as np
-from torchtext.legacy.data import Field, BucketIterator
+from torchtext.legacy.data import Field, BucketIterator, batch
 from torchtext.legacy.datasets import Multi30k
 import torch.optim as optim
 import torch.nn as nn
@@ -10,6 +11,7 @@ from sys import maxsize
 import torch
 print(torch.__version__)
 
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 spacy_ger = spacy.load('de')
 spacy_eng = spacy.load('en')
@@ -92,5 +94,57 @@ class Seq2Seq(nn.Module):
         batch_size = source.shape[1]
         target_len = target.shape[0]
         target_vocab_size = len(english.vocab)
-        pass
-    # implement the remaining part and training loop
+
+        outputs = torch.zeros(target_len, batch_size, target_vocab_size).to(device)
+        hidden, cell = self.encoder(source)
+
+        # grab start token
+        x = target[0]
+        
+        for t in range(1, target_len):
+            output, hidden, cell = self.decoder(x, hidden, cell)
+
+            outputs[t] = output
+
+            # (N, english_vocab_size)
+            best_guess = output.argmax(1)
+
+            x = target[t] if random.random() < teacher_force_ratio else best_guess
+        return outputs
+
+# Training hyperparameters
+num_epochs = 20
+learning_rate = 0.001
+batch_size = 64
+
+# Model hyperparameters
+load_model = False
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+input_size_encoder = len(german.vocab)
+input_size_decoder = len(english.vocab)
+output_size = len(english.vocab)
+encoder_embedding_size = 300
+decoder_embedding_size = 300
+hidden_size = 1024
+num_layers = 2
+enc_dropout = 0.5
+dec_dropout = 0.5
+
+# Tensorboard
+writer = SummaryWriter(f'runs/loss_plot')
+step = 0
+
+train_iterator, valid_iterator, test_iterator = BucketIterator.splits(
+    (train_data, valid_data, test_data),
+    batch_size=batch_size,
+    sort_within_batch=True,
+    sort_key = lambda x: len(x.src)
+)
+
+encoder_net = Encoder(input_size_encoder, encoder_embedding_size, hidden_size, num_layers, enc_dropout).to(device)
+encoder_net = Encoder(input_size_decoder, decoder_embedding_size, hidden_size, num_layers, dec_dropout).to(device)
+
+pad_idx = english.vocab.stoi['<pad>']
+criterion = nn.CrossEntropyLoss(ignore_index=pad_idx)
+## complete training loop
