8dd0544d51bb1ab40cde9a075c101be9ce887b46
Gladiator07 <atharvaaingle@gmail.com>
2021-04-21 17:39:43 +0000

    learning representations

---
+++ Seq_to_Seq/Learning_Phrase_Representations.py
@@ -0,0 +1,26 @@
+# Importing stuff
+import torch
+import torch.nn
+import torch.optim as optim
+
+from torchtext.legacy.datasets import Multi30k
+from torchtext.legacy.data import Field, BucketIterator
+
+import spacy
+import numpy as np
+
+import random
+import math
+import time
+
+# Setting SEED
+SEED = 1234
+
+random.seed(SEED)
+np.random.seed(SEED)
+torch.manual_seed(SEED)
+torch.cuda.manual_seed(SEED)
+torch.backends.cudnn.deterministic = True
+
+spacy_de = spacy.load('de_core_news_sm')
+spacy_en = spacy.load('en_core_web_sm')
