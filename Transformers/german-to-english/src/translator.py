ab2dd56460b7ecc9c56edf6bf31665920ec82bc1
Gladiator07 <atharvaaingle@gmail.com>
2021-05-03 17:29:40 +0000

    mask func

---
+++ Transformers/german-to-english/src/translator.py
@@ -0,0 +1,72 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import spacy
+from utils import transalte_sentence, bleu, save_checkpoint, load_checkpoint #type: ignore
+
+from torch.utils.tensorboard import SummaryWriter
+from torchtext.legacy.datasets import Multi30k
+from torchtext.legacy.data import Field, BucketIterator
+
+spacy_ger = spacy.load("de")
+spacy_en = spacy.load("en")
+
+def tokenize_ger(text):
+    return [tok.text for tok in spacy_ger.tokenizer(text)]
+
+def tokenize_eng(text):
+    return [tok.text for tok in spacy_en.tokenizer(text)]
+
+german = Field(tokenize=tokenize_ger, lower=True, init_token="<sos>", eos_token="<eos>")
+english = Field(tokenize=tokenize_eng, lower=True, init_token="<sos>", eos_token="<eos>")
+
+train_data, valid_data, test_data = Multi30k.splits(
+    exts=(".de", ".en"), fields=(german, english)
+)
+
+german.build_vocab(train_data, max_size=10000, min_freq=2)
+english.build_vocab(train_data, max_size=10000, min_freq=2)
+
+class Transformer(nn.Module):
+    def __init__(
+        self, 
+        embedding_size, 
+        src_vocab_size,
+        trg_vocab_size,
+        src_pad_idx,
+        num_heads,
+        num_encoder_layers,
+        num_decoder_layers,
+        forward_expansion,
+        dropout,
+        max_len,
+        device
+    ):
+
+        super(Transformer, self).__init__()
+        self.src_word_embedding = nn.Embedding(src_vocab_size, embedding_size)
+        self.src_position_embedding = nn.Embedding(max_len, embedding_size)
+        self.trg_word_embeddding = nn.Embedding(trg_vocab_size, embedding_size)
+        self.device = device
+        self.transformer = nn.Transformer(
+            embedding_size,
+            num_heads,
+            num_encoder_layers,
+            num_decoder_layers,
+            forward_expansion,
+            dropout
+        )
+
+        self.fc_out = nn.Linear(embedding_size, trg_vocab_size)
+        self.dropout = nn.Dropout(dropout)
+        self.src_pad_idx = src_pad_idx
+
+    def make_src_mask(self, src):
+        # src shape: (src_len, N)
+        src_mask = src.transpose(0, 1) == self.src_pad_idx
+        # (N, src_len)
+        return src_mask
+    
+    def forward(self, src, trg):
+        src_seq_length, N = src.shape
+        trg_seq_length, N = trg.shape
